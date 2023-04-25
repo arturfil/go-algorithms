fe86c9683d238c8e962eb550d6ae54f8e91397ab
arturfil <arturfil@hotmail.com>
2023-04-25 17:11:46 -0400

    read json and parsed it

--- helpers/choose_random.go
+++ helpers/choose_random.go
@@ -1,6 +1,34 @@
 package helpers
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
 type Problem struct {
-    done bool
-    name string
+    Done bool `json:"done"`
+    Title string `json:"title"`
+}
+
+type Problems struct {
+    Problems []Problem `json:"problems"`
+}
+
+func ReadJson() {
+    var problems Problems
+    file, err := os.Open("data.json")
+    if err != nil { fmt.Println(err) }
+
+    byteVal, _ := ioutil.ReadAll(file)
+    json.Unmarshal(byteVal, &problems)
+    
+    for i := 0; i < len(problems.Problems); i++ {
+        if (problems.Problems[i].Done == false) {
+            fmt.Println("Problem title ", problems.Problems[i])
+        }
+    }
+
+    defer file.Close()
 }
--- main/main.go
+++ main/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"leetcode/arrays"
+	"leetcode/helpers"
 )
 
 func main() {
-    test := []int{1,2,3,4,5}
-    println(arrays.TwoSum(test, 9))
+    // test := []int{1,2,3,4,5}
+    // println(arrays.TwoSum(test, 9))
+    helpers.ReadJson()
 }
