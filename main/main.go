9aee0966137e90bc17084ec4aafd8fe97d58f95a
arturfil <arturfil@hotmail.com>
2023-05-17 11:44:56 -0400

    added longest consecutive problem

--- main/main.go
+++ main/main.go
@@ -1,16 +1,7 @@
 package main
 
-import (
-	// "leetcode/helpers"
-	"fmt"
-	dynamicprogramming "leetcode/dynamic_programming"
-)
+import "leetcode/helpers"
 
 func main() {
-    // helpers.RandomProblem()
-    nums := []int {10,9,2,5,3,7,101,18}
-    fmt.Println(dynamicprogramming.LengthOfLIS(nums))
-    // words := []string {"one", "two", "noe", "tow"}
-    // strings.GroupAnagrams(words)
-    // strings.IsAnagram("racecar", "cracera")
+    helpers.RandomProblem()
 }
