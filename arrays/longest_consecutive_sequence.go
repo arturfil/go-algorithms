2ab0050beb199d126536167212ed14d0e512cff2
arturfil <arturfil@hotmail.com>
2023-05-20 01:41:30 -0400

    added Longest Consecutive Sequence

--- arrays/longest_consecutive_sequence.go
+++ arrays/longest_consecutive_sequence.go
@@ -1,6 +1,28 @@
 package arrays
 
-func longestConsecutive(nums []int) int {
+func LongestConsecutive(nums []int) int {
+    max_len := 0 
+    set := map[int]bool{}
+
+    for _, num := range nums { set[num] = true }
+
+    for num := range set {
+        current := num
+        sum := 0
+        // if key doesn't exist!
+        if !set[num-1] {
+            for set[current]  {
+                sum++
+                current++
+            }
+        } 
+        max_len = max(max_len, sum)
+    }
+    return max_len
+}
     
-    return 1
+func max(a int, b int) int {
+    if a > b { 
+        return a 
+    } else { return b }
 }
