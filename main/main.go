a75959900d88aba7f8c9c07a940e4117e4a24890
arturfil <arturfil@hotmail.com>
2023-06-21 10:59:46 -0400

    added maximum depth

--- main/main.go
+++ main/main.go
@@ -1,13 +1,7 @@
 package main
 
-import (
-	binary_search "leetcode/binarry_search"
-)
+import "leetcode/helpers"
 
 func main() {
-    // nums := []int {4,5,6,7,0,1}
-    nums := []int {3,1,2}
-    // arrays.LongestConsecutive(nums) 
-    // helpers.RandomProblem()
-    binary_search.FindMin(nums)
+    helpers.RandomProblem()
 }
