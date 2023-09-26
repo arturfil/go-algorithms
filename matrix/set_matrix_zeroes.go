763b5e49b851f9eed06dd2d43abd474019cfaa58
arturfil <arturfil@hotmail.com>
2023-09-26 17:34:08 -0400

    added matrix solution

--- matrix/set_matrix_zeroes.go
+++ matrix/set_matrix_zeroes.go
@@ -1,1 +1,25 @@
 package matrix
+
+func setZeroes(matrix [][]int) {
+   var colZero int = 1
+    for i := 0; i < len(matrix); i++ {
+        if matrix[i][0] == 0 {
+            colZero = 0
+        }
+        for j := 1; j < len(matrix[0]); j++ {
+            if matrix[i][j] == 0 {
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+            }
+        }
+    }
+
+    for i := len(matrix)-1; i >= 0; i-- {
+        for j := len(matrix[0])-1; j > 0; j-- {
+            if matrix[i][0]  == 0 || matrix[0][j] == 0 {
+                matrix[i][j] = 0
+            }
+        }
+        if colZero == 0 { matrix[i][0] = 0 }
+    } 
+}
