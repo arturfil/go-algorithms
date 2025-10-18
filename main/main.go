797dddf65c28533760d94ab551ddb21380e1a308
Arturo F <filioarturo@gmail.com>
2025-10-18 18:38:37 -0400

    Added Sum Two Numbers linked list.

--- main/main.go
+++ main/main.go
@@ -1,28 +1,7 @@
 package main
 
-import "leetcode/matrix"
-
-// import "leetcode/helpers"
+import "leetcode/helpers"
 
 func main() {
-	// helpers.ChooseRandomProblem()
-	// workingproblem.ProductExceptSelf([]int{1,2,3,4}) // 24, 12, 8, 6
-
-	/*
-		[
-			1, 2, 3
-			4, 5, 6
-			7, 8, 9
-		]
-
-	*/
-
-	test := [][]int{
-		{1, 2, 3, 4},
-		{5, 6, 7, 8},
-		{9, 10, 11, 12},
-		{13, 14, 15, 16},
-	}
-	matrix.SpiralOrder(test)
-
+	helpers.ChooseRandomProblem()
 }
